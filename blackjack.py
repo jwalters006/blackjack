d35c3f221f8f8fcc34e49083b283ee0c8059f227
Jeff Walters <jwalters24@gmail.com>
2015-12-12 12:49:57 -0800

    This commit would:
    Add language to import a new module (still under construction) for splitting hands.  The option to split hands (and all of the code enabling this option) would be provided in a future update.
    
    Modify the function which serves to display the history of players and select statistics for those players.  The function would now sort the data to display player history ordered by winning percentage.
    
    Redefine "blackjack" hand to mean a combination of a 10-point value card and an Ace (instead of a Jack and an Ace).
    
    Give players a 50% bonus to winning blackjack hands (21 point initial hands).
    
    Wrap monetary variables in an 'int' call, to yield whole numbers in the user displays.
    
    Create double double feature.
    
    Create surrender feature, and initialize 'surrender' variable to None.  ('surrender' variable is switched to 'True' if it is triggered during a hand).
    
    Move logic for detecting an empty wallet and auto-exiting the game into a function call.
    
    Modify the decision table (e.g, 'Hit', 'Stand', 'Double down', 'Surrender', etc.)  to display options one at a time, per line.

--- blackjack.py
+++ blackjack.py
@@ -7,6 +7,9 @@ import shelve
 # For shuffling
 import random
 
+# To import special module for hand splitting
+#from handsplit import handSplit as handSplit
+
 # Create player class
 class Player:
     def __init__(self, name, score=0, wallet=100, win=0, loss=0):
@@ -29,45 +32,65 @@ def updateDb():
 # Create function to display player history and scores
 def displayDb():
     db = shelve.open('highscores')
+    player_objects = [(db[x].name, db[x].wallet, db[x].win, db[x].loss, db[x].score) for x in db]
+    sorted_player_objects = sorted(player_objects, key=lambda x: x[2]/(x[2]+x[3]), reverse=True)
     print('\nPlayers that have played:\n')
     print(LINE_OF_DASHES)
-    for player in db:
-        print('Player name: ', player, '\t', 'Wallet: ', db[player].wallet,
-              '\t', 'Wins: ', db[player].win, '\t', 'Losses: ', db[player].loss, '\t',
-              'Best point streak: ', db[player].score)
+    for x in sorted_player_objects:
+        print('Player name: ', x[0], '\tWallet: ', int(x[1]), '\tWins: ', x[2], '\tLosses: ', x[3], '\tWinning\
+ percentage: ', '{:^.2%}'.format(x[2]/(x[2]+x[3])), sep='')
     print(LINE_OF_DASHES)
+    
+#    for player in db:
+#        print('Player name: ', player, '\t', 'Wallet: $', int(db[player].wallet),
+#              '\t', 'Wins: ', db[player].win, '\t', 'Losses: ', db[player].loss, '\t',
+#              'Highest gain in a session: $', int(db[player].score), sep='')
+#    print(LINE_OF_DASHES)
     db.close()
 
 
 # Create functions to updated running gain/loss tally and wallet, win or lose
 def updateWin():
     global wallet, tally, bet
+    if player_blackjack == True:
+        bet = bet * 1.5
     wallet += bet
     tally += bet
-    print('Your wallet increased by ', bet, '!')
-    print('Your current sessions winnings(losses) are ', tally, '.')
+    print('Your wallet increased by $', int(bet), '!', sep='')
+    print('Your current sessions winnings(losses) are $', int(tally), '.', sep='')
     player.win += 1
 
 def updateLose():
     global wallet, tally, bet
     wallet -= bet
     tally -= bet
-    print('Your wallet decreased by ', bet, '!')
-    print('Your current sessions winnings(losses) are ', tally, '.')
+    print('Your wallet decreased by $', int(bet), '!', sep='')
+    print('Your current sessions winnings(losses) are $', int(tally), '.', sep='')
     player.loss += 1
 
+# Create "double down" function
+def doubleDown():
+    global bet
+    bet = bet * 2
+
+# Create function for depleted wallet message
+def emptyWallet():
+    print('You have nothing left in your wallet. \nThe bouncer will now show you to the door...')
+    print('\nThanks for playing ', name, '!', sep='')
+    updateDb()
+    displayDb()
 
 
-
-# Create player name and create class instance
+# Create player name, create class instance and set up default surrender status
 tally = 0
 name = input('Please enter your name: ')
+surrender = None
 db = shelve.open('highscores')
 if name not in list(db.keys()):
     player = Player(name)
     print('Welcome, ', name, '!',sep='')
     wallet = player.wallet
-    print('As a new player, your wallet will start at: ', wallet, '.', sep='')
+    print('As a new player, your wallet will start at: $', int(wallet), '.', sep='')
 else:
     player = db[name]
     print('Welcome back, ', name, '!', sep='')
@@ -76,7 +99,7 @@ else:
         wallet = 50
         print('OK - looks like you\'ve depleted your wallet.  Here\'s another $50 to get your started.')
     else:
-        print('Carrying over from last time, your wallet is: ', wallet, '.', sep='')
+        print('Carrying over from last time, your wallet is: $', int(wallet), '.', sep='')
 db.close()
 
     
@@ -92,7 +115,7 @@ deck = [(card[0] + ' of '+ suit, card[1], card[2]) for
 
 
 # Create line of dashes
-LINE_OF_DASHES = '-' * 95
+LINE_OF_DASHES = '-' * 96
     
 # Create spacing
 LINE_OF_SPACES = ' ' * 25
@@ -125,8 +148,8 @@ def fullhandDisplay():
 while True:
 
     # Show current wallet and prompt for betting amount
-    print('\nCurrent wallet: ', '$', wallet)
-    print('\nGain or loss for this session: ', '$', tally, '\n')
+    print('\nCurrent wallet: $', int(wallet), sep='')
+    print('\nGain or loss for this session: $', int(tally), '\n', sep='')
     bet = input('Please enter an amount to bet: ')
 
     # Catch for anything that is either not a digit or is not between 1 and the current wallet
@@ -134,8 +157,8 @@ while True:
         try:
             bet = int(bet)
             while bet < 1 or bet > wallet:
-                print('Your current wallet is ', wallet, '.')
-                bet = int(input('Please enter a bet between 1 and your current wallet: '))
+                print('Your current wallet is $', int(wallet), '.', sep='')
+                bet = int(input('Please enter a bet between $1 and your current wallet: '))
             break
         except:
             bet = input('Please enter a digit between 1 and your current wallet: ')
@@ -190,13 +213,13 @@ while True:
     for x in player_hand:
         if 'Ace' in x[0]:
             for y in player_hand:
-                if 'Jack' in y[0]:
+                if y[1] == 10:
                     player_blackjack = True
 
     for x in opponent_hand:
         if 'Ace' in x[0]:
             for y in opponent_hand:
-                if 'Jack' in y[0]:
+                if y[1] == 10:
                     opponent_blackjack = True
 
     if player_blackjack and opponent_blackjack:
@@ -205,7 +228,7 @@ while True:
         
     elif player_blackjack:
         fullhandDisplay()
-        print('You win with a blackjack!!!')
+        print('You win with a blackjack!  You get a 1.5x bonus!!!')
         updateWin()
     elif opponent_blackjack:
         fullhandDisplay()
@@ -220,12 +243,18 @@ while True:
 
 
 
-        # This code prompts the player to hit or to stay.  If the player elects to hit, another card is popped off
-        # the game deck and put into the player's hand.  There are "if tests" to detect if the card is an Ace.  If the
-        # card is detected as being an Ace, it is set to a low Ace if the current hand points total is higher than 12,
-        # to avoid busting.
+        # This code prompts the player to hit, double down, split, or stand.  If the player elects to hit, another card is popped off
+        # the game deck and put into the player's hand.  Doubling down achieves the same thing, but doubles the original bet and
+        # restricts the player to only one additional hit.  Splitting (available if the opening hand consists of two of a kind)
+        # splits the opening hand into two seperate hands, each of which the player can make independent decisions on.  Finally, standing
+        # simply ends the turn for the player.  There are "if tests" to detect if the card is an Ace.  If the card is detected as being
+        # an Ace, it is set to a low Ace if the current hand points total is higher than 12, to avoid busting.
         while L < 22:
-            decision = input('\nType "H" to hit.  Type "S" to stand.')
+            decision = input('\nSelect a command: (type the key in parenthesis)\n\
+(H)it\t\t- Draw another card\n\
+(D)ouble down\t- Double your initial bet and draw one additional card\n\
+(S)tand\t\t- Stop drawing cards and end your hand\n\
+su(R)render\t- Forfeit this game and lose half of your initial bet\n\nCommand: ')
             if decision.upper() == 'H':
                 player_hand.append(game_deck.pop(0))
                 print('\nYou draw', player_hand[-1][0])
@@ -240,29 +269,71 @@ while True:
                     l.sort()
                     if l[-1] == 11:
                         l[-1] = 1
-                                                              
+   
                 L = sum(l)
                 handDisplay()
                 print('\nYour hand is now worth', L, 'points.')
+
+            elif decision.upper() == 'D':
+                if wallet < (bet * 2):
+                    print('Can\'t double down: your bet would exceed your current wallet.')
+                else:
+                    print('\nDoubling the original bet of $', int(bet), ' to $', int((bet * 2)), '.', sep='')
+                    doubleDown()
+                    player_hand.append(game_deck.pop(0))
+                    print('\nYou draw', player_hand[-1][0])
+                    if player_hand[-1][1] == 11:
+                        if L > 12:
+                            l.append(player_hand[-1][2])
+                        else:
+                            l.append(player_hand[-1][1])
+                    else:
+                        l.append(player_hand[-1][1])
+                    if sum(l) > 21:
+                        l.sort()
+                        if l[-1] == 11:
+                            l[-1] = 1
+                    L = sum(l)
+                    handDisplay()
+                    print('\nYour hand is now worth', L, 'points.')
+                    input('Press enter to continue...')
+                    break
+
             elif decision.upper() == 'S':
                 print('\nStanding with:', L, 'points.')
                 input('Press enter to continue...')
                 break
+
+#            elif decision.upper() == 'P':
+#                if len(player_hand) == 2 and wallet >= (bet * 2) and player_hand[0][1] != player_hand[1][1]:
+#                    handSplit(player_hand)
+#                    break
+#                else:
+#                    print('Can\t split hand: either not enough to cover bet, or cards not of same value.')
+
+            elif decision.upper() == 'R':
+                if not bet > 1:
+                    print('Sorry, you can only surrender with a bet of greater than $1.')
+                else:
+                    surrender = True
+                    break
+            
             L = sum(l)
 
 
         # If, after all of the preceding events have left a player with a hand over 21, this prints a "losing" message.  
-        if L > 21:
+        if surrender == True:
+            print('You surrender and hand over half of your bet: $', bet, ' divided by 2 is $', int((bet/2)), '.', sep='')
+            bet = int((bet/2))
+        elif L > 21:
             print('With', L, 'points, you bust!')
-         
-            
+      
         else:
             print('\nDealer\'s other card is a', opponent_hand[1][0])
             fullhandDisplay()
             print('Dealer\'s full hand shows', M, 'points.')
             print('\n')
             input('Press enter to continue...')
-
             print('\n')
 
 
@@ -299,7 +370,11 @@ while True:
 
 
         # This code evaluates final scores once more and prints an appropriate message.       
-        if 22 > L == M:
+        if surrender == True:
+            print('\nYou lose...')
+            surrender = False
+            updateLose()
+        elif 22 > L == M:
             print('\nPush!')
         elif 22 > L > M:
             print('\nYou win!!!')
@@ -317,18 +392,15 @@ while True:
     # Once the hand is finished, your wallet is checked to see if it is still positive.  If not, you are ejected from the game...
 
     if wallet < 1:
-        print('You have nothing left in your wallet. \nThe bouncer will now show you to the door...')
-        print('\nThanks for playing ', name, '!', sep='')
-        updateDb()
-        displayDb()
+        emptyWallet()
         break
        
     # Finally, the game asks if you wish to continue and exits on a 'q' or a 'Q'.
     another = input('\nWant to quit? If so, type "Q".  Press enter to continue with another game.')
     if another.upper() == 'Q':
-        print('You end the game with a total gain(loss) of: ', tally, '...')
-        print('Your wallet amount ends at: ', wallet, '...')
-        print('\nThanks for playing ', name,'!', sep='')
+        print('You end the game with a total gain(loss) of: $', int(tally), '...', sep='')
+        print('Your wallet amount ends at: $', int(wallet), '...', sep='')
+        print('\nThanks for playing, ', name,'!', sep='')
         updateDb()
         displayDb()
         break
---
+++ handsplit.py
@@ -0,0 +1,6 @@
+def handSplit(hand):
+    player_hand1 = []
+    player_hand2 = []
+    player_hand1.append(hand.pop(0))
+    player_hand2.append(hand.pop(0))
+    
